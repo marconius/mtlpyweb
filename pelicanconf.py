ef797a026bb6dc65dbb960b75edce94432fe54d9
George Peristerakis <peristerakis@gmail.com>
2012-11-15 23:46:02 -0500

    Config settings changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,13 +10,9 @@ TIMEZONE = 'America/Montreal'
 DEFAULT_LANG = 'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('Python.org', 'http://python.org'), )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/mtlpy'), )
 
 DEFAULT_PAGINATION = False
