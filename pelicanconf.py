7d56a45db57e225a32b16bbf9b3d8a0b15b0886f
Nilovna Bascunan-Vasquez <n.b.vasquez@gmail.com>
2013-04-07 21:26:51 -0400

    Add page calendar

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,6 @@ THEME_STATIC_PATHS = (['static'])
 DELETE_OUTPUT_DIRECTORY = True
 RELATIVE_URLS = True
 
-TEMPLATE_PAGES = {'styleguide.html': 'styleguide.html' }
 
 ARTICLE_URL = ARTICLE_LANG_URL = '{lang}/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = '{lang}/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
@@ -39,6 +38,5 @@ ARTICLE_LANG_SAVE_AS = ARTICLE_SAVE_AS
 
 PAGE_URL = PAGE_LANG_URL = '{lang}/{slug}/'
 PAGE_SAVE_AS = PAGE_LANG_SAVE_AS = '{lang}/{slug}/index.html'
-
 
 TEMPLATE_PAGES = {'pages/styleguide.html': 'styleguide.html',}
