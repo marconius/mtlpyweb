22e6af5ba11cbc960b82ff4392c9bf2038426854
George Peristerakis <peristerakis@gmail.com>
2013-04-08 23:50:20 -0400

    more config changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,16 +4,16 @@ import os
 
 AUTHOR = u"Montréal-Python"
 SITENAME = u"Montréal-Python"
+SITEURL = 'http://localhost:8888'
 
 TIMEZONE = 'America/Montreal'
-
 DEFAULT_LANG = 'en'
 LANGUAGES = ['en', 'fr']
 
 MARKUP = ('rst', 'md', 'html')
 
-SITEURL = '/mtlpyweb/output'
 FEED_DOMAIN = 'http://montrealpython.org'
+
 STATIC_URL = '/theme'
 
 # Blogroll
@@ -26,11 +26,10 @@ SOCIAL = (('twitter', 'https://twitter.com/mtlpy'),
 DEFAULT_PAGINATION = 10
 
 THEME = 'themes/mtlpy'
-THEME_STATIC_PATHS = (['static'])
 
+DISPLAY_PAGES_ON_MENU = False
 DELETE_OUTPUT_DIRECTORY = True
 RELATIVE_URLS = True
-
 
 ARTICLE_URL = ARTICLE_LANG_URL = '{lang}/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = '{lang}/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
@@ -39,4 +38,5 @@ ARTICLE_LANG_SAVE_AS = ARTICLE_SAVE_AS
 PAGE_URL = PAGE_LANG_URL = '{lang}/{slug}/'
 PAGE_SAVE_AS = PAGE_LANG_SAVE_AS = '{lang}/{slug}/index.html'
 
-TEMPLATE_PAGES = {'pages/styleguide.html': 'styleguide.html',}
+# TEMPLATE_PAGES = {'pages/styleguide.html': 'styleguide.html',}
+# TEMPLATE_PAGES = {'pages/en/about.html': 'en/about.html',}
